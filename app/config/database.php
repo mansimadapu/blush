682530367f104581a74f9a64290aebb0944c8842
Manasa Madapu <manasa.madapu@venuepick.com>
2014-08-26 18:26:33 +0530

    commit

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'blush',
+			'username'  => 'root',
+			'password'  => 'venuepick',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
